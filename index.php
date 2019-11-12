cfb172953641ab086b127b4a5374604bbc07e68c
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-12 12:17:31 +0100

    1.4.3. Syntaxe PHP de base
    echo et commentaires

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP & MySQL</title>
+    </head>
+    
+    <body>
+        <h1>Titre principal</h1>
+        <?php
+            //Affiche "Hello World" avec un retour à la ligne
+            echo 'Hello World <br>'; // Ceci est un commentaire PHP
+            
+            /*Affiche
+              "Bonjour le Monde
+            */
+            echo "Bonjour le Monde"; /* Ceci est un commentaire PHP*/
+        ?>
+        <p>Un paragraphe</p>
+    </body>
+</html>
