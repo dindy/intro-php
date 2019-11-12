b063feea3800311290c58087d7b78df13c8a38a4
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-12 13:22:58 +0100

    1.5.2. Les structures de langage `echo` et `print`
    Afficher des valeurs

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     <head>
         <title>Cours PHP & MySQL</title>
     </head>
-    
     <body>
         <h1>Titre principal</h1>
         <?php
-            //Affiche "Hello World" avec un retour à la ligne
-            echo 'Hello World <br>'; // Ceci est un commentaire PHP
-            
-            /*Affiche
-              "Bonjour le Monde
-            */
-            echo "Bonjour le Monde"; /* Ceci est un commentaire PHP*/
+            echo "<h2>Première instruction PHP avec echo</h2>";
+            echo 'Bonjour à tous !<br>';
+            echo  31;
+            echo "<br>J'ai ", 31, " ans.<br>";
+
+            print "<h2>Première instruction PHP avec print</h2>";
+            print 'Bonjour à tous !<br>';
+            print  31;
         ?>
         <p>Un paragraphe</p>
     </body>
