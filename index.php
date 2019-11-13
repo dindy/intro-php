3e44ff453724468d9503d11d03700532dcbbefa9
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-13 14:57:27 +0100

    2.3.3. Les opérateurs arithmétiques

--- index.php
+++ index.php
@@ -5,17 +5,27 @@
     </head>
         <h1>Titre principal</h1>
         <?php
-            $prenom = "Pierre";
-            $nom = "Giraud";
-            $age = 28;
-            $prez = "Je suis " .$prenom. " " .$nom. ", j'ai " .$age. " ans";
-            $prez2 = 'Je suis ' .$prenom. ' ' .$nom. ', j\'ai '.$age. ' ans';
+            $x = 2;
+            $y = 3;
+            $z = 4;
+            echo '$x stocke ' .$x. ', $y stocke ' .$y. ', $z stocke ' .$z. '<br>';
             
+            $a = $x + 1; //$a stocke 2 + 1 = 3
+            $b = $x + $y; //$b stocke 2 + 3 = 5
+            $c = $x - $y; //$c stocke 2 - 3 = -1
+            echo '$a stocke ' .$a. ', $b stocke ' .$b. ', $c stocke ' .$c. '<br>';
             
-            echo "Je m'appelle " .$prenom. " et j'ai " .$age. " ans <br>";
-            echo 'Je m\'appelle ' .$prenom. ' et j\'ai ' .$age. ' ans <br>';
+            $x = $x * $y; //$x stocke désormais 2 * 3 = 6
+            echo 'La variable $x stocke désormais : ' .$x. '<br>';
             
-            echo $prez. '<br>' .$prez2;
+            $z = $x / $y; //$z stocke désormais 6 / 3 = 2
+            echo 'La variable $z stocke désormais : ' .$z. '<br>';
+            
+            $m = 5 % 3; //$m stocke le reste de la division euclidienne de 5 par 3
+            echo 'Le reste de la division euclidienne de 5 par 3 est ' .$m. '<br>';
+            
+            $p = $z ** 4; //$p stocke 2^4 = 2 * 2 * 2 * 2 = 16
+            echo 'La variable $p stocke le résultat de 2 puissance 4 = ' .$p;
         ?>
         <p>Un paragraphe</p>
     </body>
