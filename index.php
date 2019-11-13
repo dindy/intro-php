a0360c1fbdd3594504953c83fbdd019b23292009
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-13 10:25:59 +0100

    1.5.3. L’usage des apostrophes et des guillemets...
    ...pour afficher des chaines de caractères

--- index.php
+++ index.php
@@ -3,17 +3,12 @@
     <head>
         <title>Cours PHP & MySQL</title>
     </head>
-    <body>
         <h1>Titre principal</h1>
         <?php
-            echo "<h2>Première instruction PHP avec echo</h2>";
-            echo 'Bonjour à tous !<br>';
-            echo  31;
-            echo "<br>J'ai ", 31, " ans.<br>";
-
-            print "<h2>Première instruction PHP avec print</h2>";
-            print 'Bonjour à tous !<br>';
-            print  31;
+            echo 'Je m\'appelle Florence mais tout le monde m\'appelle "Flo"<br>';
+            echo "Je m'appelle Florence mais tout le monde m'appelle \"Flo\"<br>";
+            print 'Je m\'appelle Florence mais tout le monde m\'appelle "Flo"<br>';
+            print "Je m'appelle Florence mais tout le monde m'appelle \"Flo\"<br>";
         ?>
         <p>Un paragraphe</p>
     </body>
