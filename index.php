11c74e6d7164521277075e3b491a6873952b6b80
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-13 16:21:45 +0100

    2.3.4. Les opérateurs d’affectation et opérateurs combinés

--- index.php
+++ index.php
@@ -5,27 +5,17 @@
     </head>
         <h1>Titre principal</h1>
         <?php
-            $x = 2;
+            $a = "Bonjour";
+            $a .= " le monde"; //$a stocke "Bonjour le monde"
+            echo '$a stocke : ' .$a. '<br>';
+            
+            $x = 5;
+            $x -= 3; //$x stocke désormais 2
+            echo '$x stocke : ' .$x. '<br>';
+            
             $y = 3;
-            $z = 4;
-            echo '$x stocke ' .$x. ', $y stocke ' .$y. ', $z stocke ' .$z. '<br>';
-            
-            $a = $x + 1; //$a stocke 2 + 1 = 3
-            $b = $x + $y; //$b stocke 2 + 3 = 5
-            $c = $x - $y; //$c stocke 2 - 3 = -1
-            echo '$a stocke ' .$a. ', $b stocke ' .$b. ', $c stocke ' .$c. '<br>';
-            
-            $x = $x * $y; //$x stocke désormais 2 * 3 = 6
-            echo 'La variable $x stocke désormais : ' .$x. '<br>';
-            
-            $z = $x / $y; //$z stocke désormais 6 / 3 = 2
-            echo 'La variable $z stocke désormais : ' .$z. '<br>';
-            
-            $m = 5 % 3; //$m stocke le reste de la division euclidienne de 5 par 3
-            echo 'Le reste de la division euclidienne de 5 par 3 est ' .$m. '<br>';
-            
-            $p = $z ** 4; //$p stocke 2^4 = 2 * 2 * 2 * 2 = 16
-            echo 'La variable $p stocke le résultat de 2 puissance 4 = ' .$p;
+            $y **= $x; //$y stocke 3^2 = 3 * 3 = 9
+            echo '$y stocke : ' .$y;
         ?>
         <p>Un paragraphe</p>
     </body>
