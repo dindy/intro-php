29278091d3310e26abdd2c85ddaea2252c97c260
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-13 11:55:32 +0100

    2.2.2. Les types de données nombre entier et nombre décimal

--- index.php
+++ index.php
@@ -5,15 +5,14 @@
     </head>
         <h1>Titre principal</h1>
         <?php
-            $prez = "Je m'appelle Pierre";
             $age = 28; //Stocke le nombre 28
-            $age2 = "28"; //Stocke la chaine de caractères "28"
+            $distance = 2.84;
             
-            echo "La variable \$age contient une valeur de type ";
+            echo 'La variable $age contient une valeur de type ';
             echo gettype($age);
             
-            echo "<br>La variable \$age2 contient une valeur de type ";
-            echo gettype($age2);
+            echo '<br>La variable $distance contient une valeur de type ';
+            echo gettype($distance);
         ?>
         <p>Un paragraphe</p>
     </body>
