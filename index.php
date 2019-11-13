e70e5e7ebb0b0849999f56892412d7a702a4ec9e
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-13 12:03:14 +0100

    2.2.4. Le type de données Null

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     </head>
         <h1>Titre principal</h1>
         <?php
-            $vrai = true;
-            $faux = false;
+            $vide = NULL;
+            $vide2;
             
-            echo 'La variable $vrai contient une valeur de type ';
-            echo gettype($vrai);
+            echo 'La variable $vide contient une valeur de type ';
+            echo gettype($vide);
             
-            echo '<br>La variable $faux contient une valeur de type ';
-            echo gettype($faux);
+            echo '<br>La variable $vide2 contient une valeur de type ';
+            echo gettype($vide2);
         ?>
         <p>Un paragraphe</p>
     </body>
