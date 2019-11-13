4a382b3d70a30c69c3bd7ecd8b81bdf361c0c296
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-13 11:59:28 +0100

    2.2.3. Le type de données booléen (Boolean)

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     </head>
         <h1>Titre principal</h1>
         <?php
-            $age = 28; //Stocke le nombre 28
-            $distance = 2.84;
+            $vrai = true;
+            $faux = false;
             
-            echo 'La variable $age contient une valeur de type ';
-            echo gettype($age);
+            echo 'La variable $vrai contient une valeur de type ';
+            echo gettype($vrai);
             
-            echo '<br>La variable $distance contient une valeur de type ';
-            echo gettype($distance);
+            echo '<br>La variable $faux contient une valeur de type ';
+            echo gettype($faux);
         ?>
         <p>Un paragraphe</p>
     </body>
