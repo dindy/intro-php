24de21cdbeff92f8a0bacf9f7b2a0fb83da8864c
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2019-11-13 11:19:10 +0100

    2.1.4. Afficher et modifier le contenu d’une variable PHP
    Modification

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
     </head>
         <h1>Titre principal</h1>
         <?php
-            $prenom = 'Sylvain';
-            $age = 31;
-
-            echo $prenom;
+            $prenom = "Sylvain";
+            $age = 28; //$age stocke le nombre 28
+            
+            echo "La variable $age contient : ";
+            echo $age;
+            echo "<br>";
+            
+            $age = 31; //$age stocke le nombre 31
+            echo "La variable $age contient : ";
+            echo $age;
         ?>
         <p>Un paragraphe</p>
     </body>
